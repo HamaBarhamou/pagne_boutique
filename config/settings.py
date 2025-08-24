df66e50fede43aa91d92c6059343afd97278b7f0
HAMA Barhamou <hamabarhamou@gmail.com>
2025-08-24 12:46:17 +0100

    feat: page d'accueil minimale (home)

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -105,24 +104,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 # --- localisation ---
-LANGUAGE_CODE = 'fr-fr'
-TIME_ZONE = 'Africa/Niamey'
+LANGUAGE_CODE = "fr-fr"
+TIME_ZONE = "Africa/Niamey"
 USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'   # pour la prod
-STATICFILES_DIRS = [BASE_DIR.parent / 'static']  # pour un dossier 'static/' à la racine
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # pour la prod
+STATICFILES_DIRS = [BASE_DIR.parent / "static"]  # pour un dossier 'static/' à la racine
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.parent / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR.parent / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
